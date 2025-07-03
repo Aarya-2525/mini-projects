94dbdbb9dc800835655c1550591d0c303a7a32e3
Aarya-2525 <aaryar025@gmail.com>
2025-07-03 22:51:19 +0530

    Initial commit: Basic emoji converter based on text

---
+++ emoji_converter.py
@@ -0,0 +1,9 @@
+message = input(">")
+words = message.split(" ")
+emojis = {":)": "😊", ":(": "😞","<3": "❤️",":D": "😄"}
+output = ""
+for word in words:
+    output += emojis.get(word,word) + " "
+print(output)
+
+
